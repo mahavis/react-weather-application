15641668454728b86bb8ef193f03166f7a029a4b
mahavis <mpn1984@gmail.com>
2022-11-20 12:04:09 +0100

    added GitHub and Netlify Link

--- src/App.js
+++ src/App.js
@@ -5,9 +5,20 @@ function App() {
     <div className="App">
       <footer>
         This project was coded by Mahalakshmi Viswanath and{" "}
-        <a href="/" target="_blank">
+        <a
+          href="https://github.com/mahavis/react-weather-application"
+          target="_blank"
+          rel="noreferrer"
+        >
           {" "}
-          open sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://ornate-arithmetic-0f2598.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          hosted on Netlify
         </a>
       </footer>
     </div>
