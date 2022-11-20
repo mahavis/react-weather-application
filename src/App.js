fed2b370feec0e7bbfda7477c872705315af48a5
mahavis <mpn1984@gmail.com>
2022-11-20 12:05:47 +0100

    correction

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           target="_blank"
           rel="noreferrer"
         >
-          {" "}
+          Open-sourced on GitHub{" "}
         </a>{" "}
         and{" "}
         <a
